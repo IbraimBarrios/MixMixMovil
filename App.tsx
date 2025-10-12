317896f80b38c47c4c9fd1803f65f0dc267fbc3e
Ibraim Barrios <barrios.party@hotmail.com>
2025-10-11 22:43:32 -0600

    Add mixmix text to home

--- App.tsx
+++ App.tsx
@@ -5,41 +5,40 @@
  * @format
  */
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
 import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
+  const styles = getStyles(isDarkMode);
 
   return (
     <SafeAreaProvider>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
+      <SafeAreaView style={styles.container}>
+        <View style={styles.viewContainer}>
+          <Text style={styles.text}>MixMix</Text>
+        </View>
+      </SafeAreaView>
     </SafeAreaProvider>
   );
 }
-
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
-
-  return (
-    <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
+const getStyles = (isDarkMode: boolean) => {
+  return StyleSheet.create({
+    container: { flex: 1 },
+    viewContainer: { paddingLeft: 12, paddingRight: 12 },
+    text: {
+      fontSize: 16,
+      color: isDarkMode ? '#fff' : '#000',
+      fontWeight: 'bold',
+    },
+  });
+};
 
 export default App;
