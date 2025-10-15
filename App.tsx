a8ab474e7ca899cb32512feb493de0179adfc264
Ibraim Barrios <barrios.party@hotmail.com>
2025-10-14 20:04:37 -0600

    Add mode dark

--- App.tsx
+++ App.tsx
@@ -8,10 +8,14 @@ import { GluestackUIProvider } from '@gluestack-ui/themed';
 import { config } from '@gluestack-ui/config';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import Routes from './src/routers';
+import { useColorScheme } from 'react-native';
 
 function App() {
+  const systemColorScheme = useColorScheme();
+  const colorMode = systemColorScheme || 'light';
+
   return (
-    <GluestackUIProvider config={config} >
+    <GluestackUIProvider config={config} colorMode={colorMode}>
       <SafeAreaProvider>
         <Routes />
       </SafeAreaProvider>
--- src/views/HomeScreen/index.tsx
+++ src/views/HomeScreen/index.tsx
@@ -1,25 +1,38 @@
 import React from 'react';
-// import {
-//   StatusBar,
-//   StyleSheet,
-//   Text,
-//   useColorScheme,
-//   View,
-// } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
-
-import { Box, Button, ButtonText, Text } from '@gluestack-ui/themed';
+import {
+  Box,
+  Button,
+  ButtonText,
+  SafeAreaView,
+  Text,
+} from '@gluestack-ui/themed';
 import { StatusBar, useColorScheme } from 'react-native';
 
 const HomeScreen = () => {
   const isDarkMode = useColorScheme() === 'dark';
-  // const styles = getStyles(isDarkMode);
 
   return (
-    <SafeAreaView>
+    <SafeAreaView
+      flex={1}
+      backgroundColor="$white"
+      sx={{ _dark: { backgroundColor: '$backgroundDark950' } }}
+    >
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <Box paddingHorizontal="$2">
-        <Text size="2xl" bold color="$black" mb="$4">
+      <Box
+        flex={1}
+        padding="$2"
+        backgroundColor="$amber400"
+        sx={{
+          _dark: { backgroundColor: '$backgroundDark950' },
+        }}
+      >
+        <Text
+          size="2xl"
+          bold
+          color="$black"
+          mb="$4"
+          sx={{ _dark: { color: '$white' } }}
+        >
           ¡MixMix funcionando! 🍹
         </Text>
         <Button>
@@ -27,25 +40,7 @@ const HomeScreen = () => {
         </Button>
       </Box>
     </SafeAreaView>
-    // <SafeAreaView>
-    //   <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-    //   <View style={styles.viewContainer}>
-    //     <Text style={styles.text}>MixMix</Text>
-    //   </View>
-    // </SafeAreaView>
   );
 };
-
-// const getStyles = (isDarkMode: boolean) => {
-//   return StyleSheet.create({
-//     container: { flex: 1, backgroundColor: isDarkMode ? '#000' : '#fff' },
-//     viewContainer: { paddingLeft: 12, paddingRight: 12 },
-//     text: {
-//       fontSize: 16,
-//       color: isDarkMode ? '#fff' : '#000',
-//       fontWeight: 'bold',
-//     },
-//   });
-// };
 
 export default HomeScreen;
