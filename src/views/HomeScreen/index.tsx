38f240145a6d3a94415565fb44a83eeec064a770
Ibraim Barrios <barrios.party@hotmail.com>
2025-10-30 21:19:47 -0600

    Add vertical scroll at home

--- src/views/HomeScreen/index.tsx
+++ src/views/HomeScreen/index.tsx
@@ -9,7 +9,7 @@ import {
   Text,
   VStack,
 } from '@gluestack-ui/themed';
-import { StatusBar, useColorScheme } from 'react-native';
+import { ScrollView, StatusBar, useColorScheme } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import DrinkImage from '../../components/DrinkImage';
 import Categories from '../../components/Categories';
@@ -27,60 +27,65 @@ const HomeScreen = () => {
       sx={{ _dark: { backgroundColor: '$backgroundDark950' } }}
     >
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <Box
-        padding="$3"
-        sx={{
-          _dark: { backgroundColor: '$backgroundDark950' },
-        }}
+      <ScrollView
+        alwaysBounceVertical={false}
+        showsVerticalScrollIndicator={false}
       >
-        {/* Secction DrinkRandom */}
-        <DrinkImage />
-        <VStack space="md" marginTop="$4">
-          <HStack justifyContent="space-between" alignItems="center">
-            <Text
-              size="xl"
-              bold
-              color="$black"
-              sx={{ _dark: { color: '$white' } }}
-            >
-              Mojito
+        <Box
+          padding="$3"
+          sx={{
+            _dark: { backgroundColor: '$backgroundDark950' },
+          }}
+        >
+          {/* Secction DrinkRandom */}
+          <DrinkImage />
+          <VStack space="md" marginTop="$4">
+            <HStack justifyContent="space-between" alignItems="center">
+              <Text
+                size="xl"
+                bold
+                color="$black"
+                sx={{ _dark: { color: '$white' } }}
+              >
+                Mojito
+              </Text>
+              <CategoryTag />
+            </HStack>
+            <Text color="$black" sx={{ _dark: { color: '$white' } }}>
+              Muddle mint leaves with sugar and lime juice. Add a splash of soda
+              water and fill the glass with cracked ice. Pour the rum and top
+              with soda water.
             </Text>
-            <CategoryTag />
-          </HStack>
-          <Text color="$black" sx={{ _dark: { color: '$white' } }}>
-            Muddle mint leaves with sugar and lime juice. Add a splash of soda
-            water and fill the glass with cracked ice. Pour the rum and top with
-            soda water.
-          </Text>
-          <Button
-            onPress={() => navigation.navigate('Detail')}
-            backgroundColor="$black"
-            sx={{ _dark: { backgroundColor: '$white' } }}
-          >
-            <ButtonText color="$white" sx={{ _dark: { color: '$black' } }}>
-              Ver detalles
-            </ButtonText>
-          </Button>
-        </VStack>
-        <Divider my="$4" />
-        {/* Secction Categories */}
-        <Categories />
-        {/* Secction Drinks */}
-        <VStack space="md" mt="$4">
-          <DrinkCard
-            name="Old Fashioned"
-            url="https://www.thecocktaildb.com/images/media/drink/vrwquq1478252802.jpg"
-          />
-          <DrinkCard
-            name="Boston Sidecar"
-            url="https://www.thecocktaildb.com/images/media/drink/qzs5d11504365962.jpg"
-          />
-          <DrinkCard
-            name="Negroni"
-            url="https://www.thecocktaildb.com/images/media/drink/qgdu971561574065.jpg"
-          />
-        </VStack>
-      </Box>
+            <Button
+              onPress={() => navigation.navigate('Detail')}
+              backgroundColor="$black"
+              sx={{ _dark: { backgroundColor: '$white' } }}
+            >
+              <ButtonText color="$white" sx={{ _dark: { color: '$black' } }}>
+                Ver detalles
+              </ButtonText>
+            </Button>
+          </VStack>
+          <Divider my="$4" />
+          {/* Secction Categories */}
+          <Categories />
+          {/* Secction Drinks */}
+          <VStack space="md" mt="$4">
+            <DrinkCard
+              name="Old Fashioned"
+              url="https://www.thecocktaildb.com/images/media/drink/vrwquq1478252802.jpg"
+            />
+            <DrinkCard
+              name="Boston Sidecar"
+              url="https://www.thecocktaildb.com/images/media/drink/qzs5d11504365962.jpg"
+            />
+            <DrinkCard
+              name="Negroni"
+              url="https://www.thecocktaildb.com/images/media/drink/qgdu971561574065.jpg"
+            />
+          </VStack>
+        </Box>
+      </ScrollView>
     </SafeAreaView>
   );
 };
