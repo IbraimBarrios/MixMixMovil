751caed717272c977e040055a38f698a6e0ab7a7
Ibraim Barrios <barrios.party@hotmail.com>
2025-11-11 14:48:03 -0600

    Show drinks of selected category

--- src/views/HomeScreen/index.tsx
+++ src/views/HomeScreen/index.tsx
@@ -1,13 +1,13 @@
 import React, { useState } from 'react';
 import { Box, Divider, SafeAreaView } from '@gluestack-ui/themed';
-import {StatusBar, useColorScheme } from 'react-native';
+import { StatusBar, useColorScheme } from 'react-native';
 import DrinkRandom from '../../features/DrinkRandom';
 import Categories from '../../features/Categories';
 import Drinks from '../../features/Drinks';
 
 const HomeScreen = () => {
   const isDarkMode = useColorScheme() === 'dark';
-  const [category, setCategory] = useState<string>('Cocktail');
+  const [category, setCategory] = useState<string | null>(null);
 
   const handleSelectCategory = (categoryName: string) => {
     setCategory(categoryName);
@@ -31,14 +31,13 @@ const HomeScreen = () => {
         <DrinkRandom />
         <Divider my="$4" />
         {/* Secction Categories */}
-        <Categories
-          defaultCategorySelected={category}
-          onCategory={handleSelectCategory}
-        />
+        <Categories onCategory={handleSelectCategory} />
         {/* Secction Drinks */}
-        <Box flex={1} mt="$4">
-          <Drinks category={category} />
-        </Box>
+        {category && (
+          <Box flex={1} mt="$4">
+            <Drinks category={category} />
+          </Box>
+        )}
       </Box>
     </SafeAreaView>
   );
