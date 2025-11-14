893f14d3a8ed632709d85e37d18a75bc5ff27060
Ibraim Barrios <barrios.party@hotmail.com>
2025-11-13 21:00:17 -0600

    Add message 'No drinks display' by category

--- src/features/Drinks/index.tsx
+++ src/features/Drinks/index.tsx
@@ -2,12 +2,13 @@ import { useCallback, useEffect, useMemo } from 'react';
 import { Box } from '@gluestack-ui/themed';
 import { API_BASE_V1 } from '../../utils/constants';
 import { DrinkCategory } from '../../types/drink';
-import DrinkCard from '../../components/DrinkCard';
-import useGetData from '../../hooks/useGetData';
 import { FlatList } from 'react-native';
+import useGetData from '../../hooks/useGetData';
+import DrinkCard from '../../components/DrinkCard';
+import EmptyResultMessage from '../../components/EmptyResultMessage';
 
 type DrinkResponse = {
-  drinks: DrinkCategory[];
+  drinks: DrinkCategory[] | 'no data found';
 };
 
 type DrinksProps = {
@@ -18,12 +19,21 @@ const Drinks = ({ category }: DrinksProps) => {
   const { data, fetchData } = useGetData<DrinkResponse>();
 
   useEffect(() => {
-    fetchData(`${API_BASE_V1}/filter.php?c=${category}`);
+    fetchData(`${API_BASE_V1}/filter.php?c=${encodeURIComponent(category)}`);
   }, [category]);
 
   const Separator = useCallback(() => <Box height="$4" />, []);
 
-  const drinks = useMemo(() => data?.drinks || [], [data]);
+  const drinks = useMemo(() => {
+    if (data?.drinks === 'no data found') {
+      return [];
+    }
+
+    return data?.drinks || [];
+  }, [data]);
+
+  if (drinks.length === 0)
+    return <EmptyResultMessage text="No se mostraron bebidas" />;
 
   return (
     <FlatList
