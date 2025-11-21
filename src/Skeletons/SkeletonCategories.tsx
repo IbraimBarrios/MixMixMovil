4d6913c4042dfb3dd62ca1ee4304a24be3d39f6d
Ibraim Barrios <barrios.party@hotmail.com>
2025-11-20 22:25:58 -0600

    Skeleton in dark mode of catefories

--- src/Skeletons/SkeletonCategories.tsx
+++ src/Skeletons/SkeletonCategories.tsx
@@ -1,12 +1,17 @@
+import { useColorScheme } from 'react-native';
 import { Box, HStack } from '@gluestack-ui/themed';
+import { SkeletonColorsBg } from '../theme/colors';
 
 const SkeletonCategories = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+  const bgColor = isDarkMode ? SkeletonColorsBg.dark : SkeletonColorsBg.light;
+
   return (
     <HStack space="md">
-      <Box w="$20" h="$10" bg="$trueGray200" rounded="$sm" />
-      <Box w="$24" h="$10" bg="$trueGray200" rounded="$sm" />
-      <Box w="$24" h="$10" bg="$trueGray200" rounded="$sm" />
-      <Box w="$16" h="$10" bg="$trueGray200" rounded="$sm" />
+      <Box w="$20" h="$10" bg={bgColor} rounded="$sm" />
+      <Box w="$24" h="$10" bg={bgColor} rounded="$sm" />
+      <Box w="$24" h="$10" bg={bgColor} rounded="$sm" />
+      <Box w="$16" h="$10" bg={bgColor} rounded="$sm" />
     </HStack>
   );
 };
