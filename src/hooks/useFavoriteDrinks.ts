8d353ac56c8c86c4fe728fe469924e2b1bba8cc6
Ibraim Barrios <barrios.party@hotmail.com>
2025-11-26 21:30:12 -0600

    Check favorite status

--- src/hooks/useFavoriteDrinks.ts
+++ src/hooks/useFavoriteDrinks.ts
@@ -1,14 +1,39 @@
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 import { getFavorites, saveFavorites } from '../utils/utils';
+import { useFocusEffect } from '@react-navigation/native';
+
+const isFavorite = async (idDrink: string) => {
+  const favoriteDrinks: string[] = await getFavorites();
+
+  return favoriteDrinks.includes(idDrink);
+};
 
 const useFavoriteDrinks = (idDrink: string) => {
   const [isFavoriteDrink, setIsFavoriteDrink] = useState<boolean>(false);
 
+  useFocusEffect(
+    useCallback(() => {
+      let isMounted = true;
+
+      const checkStatus = async () => {
+        const favoriteStatus = await isFavorite(idDrink);
+
+        if (isMounted) {
+          setIsFavoriteDrink(favoriteStatus);
+        }
+      };
+
+      checkStatus();
+
+      return () => {
+        isMounted = false;
+      };
+    }, [idDrink]),
+  );
+
   const toggleFavoriteDrink = async () => {
-    const favoriteDrinks = await getFavorites();
+    const favoriteDrinks: string[] = await getFavorites();
     const drinkIndex = favoriteDrinks.indexOf(idDrink);
-    console.log(favoriteDrinks);
-    console.log(drinkIndex);
 
     if (drinkIndex !== -1) {
       favoriteDrinks.splice(drinkIndex, 1);
