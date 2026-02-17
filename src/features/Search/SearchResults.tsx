8f64d7bbe44b576d10350d6b171c2c079aa21788
Ibraim Barrios <barrios.party@hotmail.com>
2026-02-16 21:33:15 -0600

    Add error message and no search result

--- src/features/Search/SearchResults.tsx
+++ src/features/Search/SearchResults.tsx
@@ -1,13 +1,15 @@
-import { Box, Text } from '@gluestack-ui/themed';
 import { useCallback, useEffect, useMemo } from 'react';
-import useGetData from '../../hooks/useGetData';
+import { FlatList } from 'react-native';
+import { Box, Text } from '@gluestack-ui/themed';
 import { API_BASE_V1 } from '../../utils/constants';
 import { Drink } from '../../types/drink';
-import { FlatList } from 'react-native';
+import useGetData from '../../hooks/useGetData';
 import DrinkCard from '../../components/DrinkCard';
+import EmptyResultMessage from '../../components/EmptyResultMessage';
+import RequestErrorMessage from '../../components/RequestErrorMessage';
 
 type SearchResponse = {
-  drinks: Drink[] | null;
+  drinks: Drink[] | null | 'no data found';
 };
 
 type SearchResultsProps = {
@@ -15,7 +17,7 @@ type SearchResultsProps = {
 };
 
 const SearchResults = ({ search }: SearchResultsProps) => {
-  const { data, fetchData } = useGetData<SearchResponse>();
+  const { data, error, fetchData } = useGetData<SearchResponse>();
 
   const Separator = useCallback(() => <Box height="$4" />, []);
 
@@ -23,7 +25,14 @@ const SearchResults = ({ search }: SearchResultsProps) => {
     fetchData(`${API_BASE_V1}/search.php?s=${search}`);
   }, [search]);
 
-  const drinks = useMemo(() => data?.drinks || [], [data?.drinks]);
+  const drinks = useMemo(() => data?.drinks, [data?.drinks]);
+
+  if (error) return <RequestErrorMessage />;
+
+  if (!drinks || drinks === 'no data found')
+    return (
+      <EmptyResultMessage text="No se encontraron resultados en la búsqueda" />
+    );
 
   return (
     <>
